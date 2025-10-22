35ef7e67e45a17d6e89207724f977da03ac7f179
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-10-22 09:14:14 +0200

    updated my github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer>
       <p>&copy; 2024 Poom Portfolio. All rights reserved.</p>
       <div className="social-links">
-        <a href="https://github.com/yourusername" target="_blank">
+        <a
+          href="https://github.com/Poomsanthong?tab=repositories"
+          target="_blank"
+        >
           <i className="fab fa-github"></i>
         </a>
         <a href="https://www.linkedin.com/in/yourusername" target="_blank">
